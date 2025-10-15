7f9cc4ba8cdfdc7e1f5787d84215b90838f4ffc3
Maksim Nikulnikov <5286433@gmail.com>
2025-10-15 09:29:21 +0300

    [FIX] Update camera.

--- src/World/components/camera.js
+++ src/World/components/camera.js
@@ -1,8 +1,8 @@
 import { PerspectiveCamera } from 'three';
 
 function createCamera() {
-  const camera = new PerspectiveCamera(35, 1, 0.1, 100);
-  camera.position.set(-5, 5, 7);
+  const camera = new PerspectiveCamera(35, 1, 0.1, 200);
+  camera.position.set(-3, 50, 50);
   return camera;
 }
 
