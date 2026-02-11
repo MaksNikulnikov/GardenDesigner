cde20b562500a848348179ad863dc3e96b49d4d4
Nikulnikov Maksim <5286433@gmail.com>
2026-02-11 19:53:59 +0300

    fix(tutorial): remove delay after corn harvest phase

--- src/World/tutorial/steps/Step3.js
+++ src/World/tutorial/steps/Step3.js
@@ -8,13 +8,11 @@ export class Step3 extends TutorialStep {
 
     this._phase = "waitGrow";
     this._harvestMessageShown = false;
-    this._timer = 0;
     this._cameraFocused = false;
   }
 
-  update(delta) {
+  update() {
     const { game, ui } = this.manager;
-    this._timer += delta;
 
     switch (this._phase) {
       case "waitGrow": {
@@ -38,14 +36,6 @@ export class Step3 extends TutorialStep {
         const anyCornLeft = game.entities.entities.some((e) => e.type === "corn");
         if (!anyCornLeft && !this._harvestMessageShown) {
           this._harvestMessageShown = true;
-          this._phase = "done";
-          this._timer = 0;
-        }
-        break;
-      }
-
-      case "done": {
-        if (this._timer > 2) {
           this.isComplete = true;
         }
         break;
